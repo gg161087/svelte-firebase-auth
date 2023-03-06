16b49134daaf687d949e86a6acaed7b8891e03a4
Gonzalo <gonzalo.gonzalez@sg.gba.gob.ar>
2023-03-05 22:47:33 -0300

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYHVKPC1I_ImZMAUXE7YmDnXqK4khRZ_U",
+  authDomain: "svelte-firebase-auth-6143b.firebaseapp.com",
+  projectId: "svelte-firebase-auth-6143b",
+  storageBucket: "svelte-firebase-auth-6143b.appspot.com",
+  messagingSenderId: "574993693708",
+  appId: "1:574993693708:web:bbc0a6bd9bff4919d54229"
+};
+
+const app = initializeApp(firebaseConfig);
